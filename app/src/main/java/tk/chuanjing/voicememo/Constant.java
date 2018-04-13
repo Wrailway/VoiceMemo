b30543ccb709dcdc03df896cb0f2b8c35d8699e7
gcj <longgongsanri@163.com>
2018-04-14 00:10:21 +0800

    整理常量类

--- app/src/main/java/tk/chuanjing/voicememo/Constant.java
+++ app/src/main/java/tk/chuanjing/voicememo/Constant.java
@@ -1,14 +1,17 @@
 package tk.chuanjing.voicememo;
 
 /**
+ * 本工程的常量类
+ *
  * Created by ChuanJing on 2018/4/8.
  */
-
 public class Constant {
     /** 路径之间的分隔符 */
     public static final String IMG_OR_VIDEO_PATH_SEPARATE = "###";
 
+    /** 存放语音的路径 */
     public static final String VOICE_PATH = "/VoiceMemo/Voice";
 
+    /** 存放图片的路径 */
     public static final String IMAGE_PATH = "/VoiceMemo/Image";
 }
